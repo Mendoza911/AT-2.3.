6f6e193fd690131930e981afac14ff47603a337a
Kirill Markov <isikir668@gmail.com>
2023-06-21 09:20:48 +0300

    Task 2. With Success second try.

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -14,6 +14,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("123"));
+                .body("data", equalTo("June"));
     }
 }
